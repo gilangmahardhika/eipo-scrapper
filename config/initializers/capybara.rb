1e13acb55fd0dedd95d70b5deb2c600a11418084
gilang mahardhika <gilangmahardhika@gmail.com>
2024-01-25 21:46:23 +0700

    set higher timeout

--- config/initializers/capybara.rb
+++ config/initializers/capybara.rb
@@ -1,10 +1,17 @@
 Capybara.register_driver :selenium do |app|
+
+#   options = ::Selenium::WebDriver::Chrome::Options.new
+#   if Rails.env.production?
+#     options.binary = '/usr/bin/google-chrome'
+#   end
+#   Capybara::Selenium::Driver.new(app, browser: :chrome, options: browser_options)
+# end
   Capybara::Selenium::Driver.new(app, browser: :chrome)
   #options: Selenium::WebDriver::Chrome::Options.new(args: %w[headless disable-gpu]))
 end
 Capybara.javascript_driver = :chrome
 Capybara.configure do |config|
-  config.default_max_wait_time = 10 # seconds
+  config.default_max_wait_time = 120 # seconds
   config.default_driver = :selenium
 end
 # Visit
