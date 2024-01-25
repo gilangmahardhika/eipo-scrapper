bb33260c6fc932c550e442e9ad49819a0187b9d0
gilang mahardhika <gilangmahardhika@gmail.com>
2024-01-26 02:23:59 +0700

    add no sandbox chrome

--- config/initializers/capybara.rb
+++ config/initializers/capybara.rb
@@ -7,9 +7,14 @@ Capybara.register_driver :selenium do |app|
 #   Capybara::Selenium::Driver.new(app, browser: :chrome, options: browser_options)
 # end
   client = Selenium::WebDriver::Remote::Http::Default.new
+
+  # client = Selenium::WebDriver::Chrome::Options.new(args: [
+  # 'no-sandbox', 'disable-setuid-sandbox',
+  # 'disable-dable-dev-shm-usage'])
+
   client.read_timeout = 120 # seconds
-  Capybara::Selenium::Driver.new(app, browser: :chrome, http_client: client)
-  #options: Selenium::WebDriver::Chrome::Options.new(args: %w[headless disable-gpu]))
+  Capybara::Selenium::Driver.new(app, browser: :chrome, http_client: client,
+  options: Selenium::WebDriver::Chrome::Options.new(args: %w[no-sandbox]))
 end
 Capybara.javascript_driver = :chrome
 Capybara.configure do |config|
