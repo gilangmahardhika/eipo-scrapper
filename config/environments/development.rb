7736bf04a6038c1c2c2b84736988db1bb8d46234
gilang mahardhika <gilangmahardhika@gmail.com>
2024-01-26 18:00:55 +0700

    add hosts on development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,5 +60,5 @@ Rails.application.configure do
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
 
-  config.host = "ipo.builtbygilang.my.id"
+  config.hosts = "ipo.builtbygilang.my.id"
 end
